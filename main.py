ac641263e60dc6803a009c811231c7621bc6cfe9
kdaespir <krisespiritu5@gmail.com>
2022-07-23 17:53:45 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,38 +1,53 @@
 import pandas as pd
 import numpy as np
 
-data = pd.read_csv(r"D:\Users\Owner\Desktop\Python Projects\AirPlane Trends\flights.csv")
+
+
 def get_stats(df):
-    x = df[df.columns[6:]]
-    numeric_data = df.columns[6:]
+    '''
+    Gets various descriptive statistics of the dataset. incuding mean, quantile, standard deviation and variance
+    '''
+    numeric_data = df.columns
     means = []
     sd = []
     var = []
     p0, p25, p50, p75, p100 = [], [], [], [], []
 
     for item in numeric_data:
-        means += [np.mean(x[item])]
-        sd += [np.std(x[item])]
-        p0 += [np.quantile(x[item], 0)]
-        p25 += [np.quantile(x[item], .25)]
-        p50 += [np.quantile(x[item], .50)]
-        p75 += [np.quantile(x[item], .75)]
-        p100 += [np.quantile(x[item], 1)]
-        var += [np.var(x[item])]
+        means += [np.mean(df[item])]
+        sd += [np.std(df[item])]
+        p0 += [np.quantile(df[item], 0)]
+        p25 += [np.quantile(df[item], .25)]
+        p50 += [np.quantile(df[item], .50)]
+        p75 += [np.quantile(df[item], .75)]
+        p100 += [np.quantile(df[item], 1)]
+        var += [np.var(df[item])]
         pass
     all_data = list(zip(means, sd, var, p0, p25, p50, p75, p100))
     all_data = pd.DataFrame(all_data)
     all_data.columns = ["Mean", "Std dev", "Variance", "P0", "P25", "P50", "P75", "P100"]
-    all_data.index = df.columns[6:]
+    all_data.index = df.columns
     return all_data
 
 def by_dates(df):
     dates = df["Fly Date"].unique()
     flight_counts = []
+    dist_traveled = []
     for item in dates:
-        flight_counts += [df.loc[df["Fly Date"] == item].shape[0]]
+        flight_counts += [df.loc[df["Fly Date"] == item].shape[0]] 
+
+        dist_traveled = df.loc[df["Fly Date"] == item]["Distance"]
+        print(len(dist_traveled), len(flight_counts))
+        
     # test = df.loc[]
     return flight_counts
 
-x = by_dates(data)
-print(x)
+def main():
+# Loads the dataset into the variable data
+    data = pd.read_csv(r"D:\Users\Owner\Desktop\Python Projects\AirPlane Trends\flights.csv")
+    numeric_data = data[data.columns[6:]]
+    print(numeric_data)
+
+main()
+# x = by_dates(data)
+# print(x)
